91e7f5c734aed3460cecc1a5c5f63ca8f2ae5bcd
Suparn Gupta <gupta.suparn@gmail.com>
2015-08-28 16:42:18 -0700

    Checking in  sample project

---
+++ test/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Suparn Gupta on 8/27/15.
+//  Copyright (c) 2015 Suparn Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.estimatedRowHeight = 129
+        tableView.rowHeight = UITableViewAutomaticDimension
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = tableView.dequeueReusableCellWithIdentifier("cell") as! UITableViewCell
+//        cell.textLabel?.text = "Hello"
+        return cell
+    }
+}
+
